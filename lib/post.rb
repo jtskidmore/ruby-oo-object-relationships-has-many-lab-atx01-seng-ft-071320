3bea78e57dc55ac7f407850d1d44a14fe0ded6a9
JT Skidmore <johntoddskidmore@gmail.com>
2020-07-15 21:23:08 +0000

    Done.

---
+++ lib/post.rb
@@ -0,0 +1,30 @@
+
+
+class Post
+
+  attr_accessor :title, :author
+
+  @@all = []
+
+  def initialize (title)
+    @title = title
+    save
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def author_name
+    if self.author
+      self.author.name
+    elsif self.author == nil
+      nil
+    end
+  end
+
+end
